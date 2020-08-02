c3020853f8c3df5d0d720ec6423420fa8f068eb8
Subin Shrestha <developer.shrestha@gmail.com>
2020-08-02 23:14:34 +0545

    update quiz admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,18 +1,26 @@
 from django.contrib import admin
 
-from quiz.models import Question, Choice, Answer
+from quiz import models
 
 
-@admin.register(Question)
+@admin.register(models.Movie)
+class MovieAdmin(admin.ModelAdmin):
+	list_display = ('title', 'kind', 'year')
+
+@admin.register(models.Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'score')
+	list_display = ('title', 'score')
 
 
-@admin.register(Choice)
+@admin.register(models.Choice)
 class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('question', 'value', 'is_correct')
+	list_display = ('question', 'value', 'is_correct')
 
-@admin.register(Answer)
+@admin.register(models.Answer)
 class AnswerAdmin(admin.ModelAdmin):
-    list_display = ('user', 'question')
+	list_display = ('user', 'question')
 
+
+@admin.register(models.UserAggregate)
+class UserAggregateAdmin(admin.ModelAdmin):
+	list_display = ('user', 'total_question', 'total_score')
