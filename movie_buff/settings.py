244bc5f3e76982dacad7f8a97112d9d89a628e29
Subin Shrestha <developer.shrestha@gmail.com>
2020-08-02 17:34:47 +0545

    set token as default auth

--- movie_buff/settings.py
+++ movie_buff/settings.py
@@ -107,6 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# REST FRAMEWORK GLOBAL SETTINGS
+REST_FRAMEWORK = {
+	'DEFAULT_AUTHENTICATION_CLASSES': (
+		'rest_framework.authentication.TokenAuthentication',
+	),
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
